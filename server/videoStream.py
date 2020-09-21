f0316c71e72b558b871740b1ab41830c12010e7a
FabioFusimoto <fabiofusimoto.pires@hotmail.com>
2020-09-21 16:44:32 -0300

    Raw hand detection

--- server/videoStream.py
+++ server/videoStream.py
@@ -1,12 +1,21 @@
+import handDetection
 import cv2.cv2 as cv2
 
-from cam import Cam
-
-cam = Cam(camIndex=1)
+cap = cv2.VideoCapture(1)
+hist = handDetection.capture_histogram(source=1)
 
 while True:
-    image = cam.read()
+    ret, frame = cap.read()
+    
+    # detect the hand
+    hand = handDetection.detect_hand(frame, hist)
+    
+    # plot the fingertips
+    for fingertip in hand.fingertips:
+        cv2.circle(hand.outline, fingertip, 5, (0, 0, 255), -1)
 
-    cv2.imshow('Image capturada', image)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow("handDetection", hand.outline)
+    
+    k = cv2.waitKey(5)
+    if k == ord('q'):
         break
