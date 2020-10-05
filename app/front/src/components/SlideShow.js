ccd19fb2d8286d5e9befe2fef19df006b99ddff3
Caio Teixeira <tex.caio@gmail.com>
2020-10-05 15:26:34 -0300

    class components -> hooks

--- app/front/src/components/SlideShow.js
+++ app/front/src/components/SlideShow.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import dog1 from '../img/dog1.jpg'
 import dog2 from '../img/dog2.jpeg'
@@ -6,40 +6,34 @@ import dog3 from '../img/dog3.jpg'
 
 import './slideshow.css'
 
-class SlideShow extends React.Component {
-    state = {
-        images: [dog1, dog2, dog3],
-        channel: 0
-    }
+export default function SlideShow() {
+    const [images, setImages] = useState([dog1, dog2, dog3])
+    const [channel, setChannel] = useState(0)
 
-    onClickPrevious = () => {
-        if (this.state.channel === 0) {
-            this.setState({channel: this.state.images.length - 1})
+    const onClickPrevious = () => {
+        if (channel === 0) {
+            setChannel(images.length - 1)
         } else {
-        this.setState({channel: this.state.channel - 1})
+            setChannel(channel - 1)
         }
     }
 
-    onClickNext = () => {
-        if (this.state.channel === this.state.images.length - 1) {
-            this.setState({channel: 0})
+    const onClickNext = () => {
+        if (channel === images.length - 1) {
+            setChannel(0)
         } else {
-        this.setState({channel: this.state.channel + 1})
+            setChannel(channel + 1)
         }
     }
 
-    render() {
-        return (
+    return (
+        <div>
+            <h1>Canais</h1>
+            <img className={'channel'} src={images[channel]} alt={'Pictures of some very cute dogs'} />
             <div>
-                <h1>Canais</h1>
-                <img className={'channel'} src={this.state.images[this.state.channel]} alt={'Pictures of some very cute dogs'} />
-                <div>
-                    <button onClick={this.onClickPrevious}> Anterior </button>
-                    <button onClick={this.onClickNext}> Próximo </button>
-                </div>
+                <button onClick={onClickPrevious}> Anterior </button>
+                <button onClick={onClickNext}> Próximo </button>
             </div>
-        )
-    }
+        </div>
+    )
 }
-
-export default SlideShow;
